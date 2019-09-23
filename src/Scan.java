8cccfbe27b0e8897009e9fe8cc82240963cd100c
Jacob's Laptop <jxv3386@rit.edu>
2019-09-23 17:24:35 -0400

    didnt do much

--- src/Scan.java
+++ src/Scan.java
@@ -1,20 +1,14 @@
 /**
  * Interpreter project
  * Scanner class?
- * @author: Jacob Verdesi (jxv3386@rit.edu()
+ * @author: Jacob Verdesi jxv3386@rit.edu()
  */
 
 import java.io.*;
 import java.util.*;
 
-import java.util.logging.Logger;
-
-import static java.lang.Character.getNumericValue;
-import static java.lang.Character.isLetter;
-
 public class Scan {
     static private final String DELIMITER = "((?<=%1$s)|(?=%1$s))";
-
     /**
      * Takes in file name reads line by line
      * @param filename
@@ -49,6 +43,13 @@ public class Scan {
     private static List<String> tokenizer(List<String> lines){
         List<String> tokens = new ArrayList<String>();
         int lineNum=0;
+
+//        for (String line2: lines){
+//            String[] tok2= line2.split("\\s\"");
+//            printList(List.of(tok2));
+//        }
+//
+//        System.out.println();
         for (String line:lines){
             String[] tok=line.split(String.format(DELIMITER,"[()+\\-\\*/^=,;]"));
             for (String to:tok) {
@@ -57,6 +58,24 @@ public class Scan {
             }
         }
         tokens.removeIf(String::isEmpty);
+//        for (int i = 0; i < tokens.size(); i++) {
+//            String token=tokens.get(i);
+//            System.out.println("token: "+token);
+//            if(token.charAt(0)=='"' && token.charAt(token.length()-1)!='"'){
+//                for (int j = 0; j < tokens.subList(i,tokens.size()-1).size(); j++) {
+//                    List<String> tokens2 = new ArrayList<String>();
+//                    String token2=tokens.get(i+j);
+//                    System.out.println("token2: "+token2);
+//                    tokens2.add(token2);
+//                    tokens.remove(token2);
+//                    System.out.println();
+//                    if(token2.charAt(token2.length()-1)=='"'){
+//                        tokens.addAll(i,tokens2);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
         return tokens;
     }
 
@@ -75,7 +94,7 @@ public class Scan {
     }
 
     public static void main(String[] args){
-        List<String> a = readFile("src/program.j");
+        List<String> a = readFile("src/program1.j");
         List<String> tokens=tokenizer(a);
         printList(tokens);
 
