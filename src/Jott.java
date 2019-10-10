a2f1a83ef66b5b9e9c27e535ff1287fc11184b77
Nick Gene <ngs1112@rit.edu>
2019-10-10 08:27:57 -0400

    Reformatted main class to work as specified in Phase1 writeup (callable from terminal with 'java Jott file.j' Added copies of parse and grammar file to src and main folder (terminal only works with file in src while intellij only works with file in main folder)

--- src/Jott.java
+++ src/Jott.java
@@ -1,16 +1,17 @@
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class Jott {
+
     public static void main(String[] args){
-        List<String> prog = readFile.readFile("src/program.j");
-        List<String> table = readFile.readFile("src/LALR(1) parse table");
-        List<String> rules = readFile.readFile("src/GRAMMAR");
+        List<String> prog = readFile.readFile(args[0]);
+        List<String> table = readFile.readFile("LALR(1) parse table");
+        List<String> rules = readFile.readFile("GRAMMAR");
         List<List<String>> matrix = Parser.parseTable(table);
 
         List<List<Map.Entry<String, TERMINAL>>> tokens=Tokenizer.dfaTokenizer(prog);
-        //System.out.println(tokens);
-        //Tokenizer.printTokens(tokens);
+        System.out.println(tokens);
+        Tokenizer.printTokens(tokens);
         Tokenizer.printTerminals(tokens);
         Parser.printTable(matrix);
         System.out.println();
